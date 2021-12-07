05d0ee5ebe222910efb5dad5a9e225a33dae5314
Andrew Huffman <55068248+bbiscuit@users.noreply.github.com>
2021-12-07 18:03:10 -0500

    Pointer operators.

---
+++ lite/list/List.h
@@ -0,0 +1,55 @@
+#pragma once
+
+
+namespace lite {
+
+    /**
+     * @brief A generic rank-based list.
+     * 
+     * 
+     * @tparam T The type of the list.
+     */
+    template<typename T>
+    class List {
+
+        public:
+
+        /**
+         * @brief Dereference operator. The difference
+         * between this and the indexing operator is
+         * checking: this does not have runtime checking!
+         * 
+         * @return T& The first element in the list.
+         */
+        T& operator*() {
+            return *_data;
+        }
+
+        /**
+         * @brief Pointer addition operator. There is no
+         * bound checking on this!
+         * 
+         * @param i The index to add.
+         * @return T* The pointer to return.
+         */
+        T* operator+(unsigned int i) {
+            return _data + i;
+        }
+
+        /**
+         * @brief Gets the size of the list.
+         * 
+         * @return The size.
+         */
+        unsigned int size() const {
+            return _size;
+        }
+
+        private:
+
+        T* _data;
+        unsigned int _size;
+
+    };
+
+}
